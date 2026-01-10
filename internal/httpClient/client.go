f16a8717dde3c48c6bb5161d01ff65d6c8da2a95
batuhan <batuhan_kanra_60@hotmail.com>
2026-01-10 17:48:04 +0300

    http request updated (#2)

--- internal/httpClient/client.go
+++ internal/httpClient/client.go
@@ -5,50 +5,57 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"strings"
 	"time"
 )
 
-func Do(method, url string, headers map[string]string, body string) (string, error) {
+func Do(method, url string, headers map[string]string, body string) error {
 	var reader io.Reader
 	if body != "" {
 		reader = bytes.NewBufferString(body)
 	}
+
 	req, err := http.NewRequest(method, url, reader)
 	if err != nil {
-		return "", err
+		return err
 	}
+
 	for k, v := range headers {
 		req.Header.Set(k, v)
 	}
+
 	client := &http.Client{
 		Timeout: 15 * time.Second,
 	}
+
 	start := time.Now()
 	res, err := client.Do(req)
 	if err != nil {
-		return "", err
+		return err
 	}
 	defer res.Body.Close()
+
 	elapsed := time.Since(start)
 	b, err := io.ReadAll(res.Body)
 	if err != nil {
-		return "", err
+		return err
 	}
-	var out strings.Builder
 
-	out.WriteString(fmt.Sprintf("STATUS %d %s (%dms)\n\n",
-		res.StatusCode,
-		res.Status,
-		elapsed.Milliseconds()))
-	out.WriteString("Headers:\n")
-	for k, v := range res.Header {
-		out.WriteString(fmt.Sprintf("  %s: %s\n", k, strings.Join(v, ",")))
+	fmt.Println("*********** REQUEST ***********")
+	fmt.Printf("%s %s\n", method, url)
 
+	if body != "" {
+		fmt.Println("Body:")
+		fmt.Println(body)
 	}
-	out.WriteString("\nBody:\n")
-	out.Write(b)
 
-	return out.String(), nil
+	fmt.Println()
 
+	// RESPONSE
+	fmt.Println("*********** RESPONSE ***********")
+	fmt.Printf("STATUS %s (%dms)\n\n", res.Status, elapsed.Milliseconds())
+
+	fmt.Println("Body:")
+	fmt.Println(string(b))
+
+	return nil
 }
